a48518ad8693f0f6cb471cb1bd11d8ab345867ef
Travis Scott <wilscott127@gmail.com>
2024-01-12 18:03:44 -0600

    first commit

---
+++ exchange.js
@@ -0,0 +1,45 @@
+const amount = document.getElementById('amount');
+
+const currency = document.getElementById('currency');
+const convert = document.getElementById('convert');
+const result = document.getElementById('result');
+
+
+const apiUrl='https://api.api-ninjas.com/v1/exchangerate?pair=USD_'
+
+convert.addEventListener('click', () => {
+    const amountTotal = amount.value;
+    const currencyTotal = currency.value;
+    const url = apiUrl + currencyTotal;
+
+    fetch(url, {
+        
+        headers: {
+            'X-Api-Key': API_KEY 
+
+        }
+    })
+    .then(response => {
+
+        
+        return response.json()
+        
+
+
+     } )
+    
+    .then(data => {
+        
+        const rate = data.exchange_rate;
+        const totalConverted = amountTotal * rate;
+        result.innerHTML = `${amount.value} ${currency.value} = ${totalConverted.toFixed(2)} USD`;
+
+    })
+    .catch(error => {
+        console.error('Request failed:', error);
+        result.innerHTML = 'An error occurred please try again later.'
+        
+    })
+
+})
+
